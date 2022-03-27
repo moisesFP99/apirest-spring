7274abad0f990874b856b32b9b375259abf7477f
Moises <moisesxd8@gmail.com>
2022-03-27 19:05:33 -0300

    apontando pro banco de dados

--- src/main/java/com/example/restapi/CidadeREST.java
+++ src/main/java/com/example/restapi/CidadeREST.java
@@ -6,25 +6,38 @@ import com.example.restapi.database.RepositorioCidade;
 import com.example.restapi.entidade.Cidade;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/cidade")
 public class CidadeREST {
     @Autowired
     private RepositorioCidade repositorioCidade;
 
+    @GetMapping
     public List<Cidade> listar(){
         return repositorioCidade.findAll();
     }
 
-    public void salvar(Cidade cidade){
+    @PostMapping
+    public void salvar(@RequestBody Cidade cidade){
         repositorioCidade.save(cidade);
     }
 
-    public void alterar(Cidade cidade){
+    @PutMapping
+    public void alterar(@RequestBody Cidade cidade){
         if(cidade.getIdCidade() > 0)
         repositorioCidade.save(cidade);
     }
 
-    public void excluir(Cidade cidade){
+    @DeleteMapping
+    public void excluir(@RequestBody Cidade cidade){
         repositorioCidade.delete(cidade);
     }
 }
--- src/main/java/com/example/restapi/EstadoREST.java
+++ src/main/java/com/example/restapi/EstadoREST.java
@@ -6,25 +6,38 @@ import com.example.restapi.database.RepositorioEstado;
 import com.example.restapi.entidade.Estado;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/estado")
 public class EstadoREST {
     @Autowired
     private RepositorioEstado repositorioEstado;
 
+    @GetMapping
     public List<Estado> listar(){
         return repositorioEstado.findAll();
     }
 
-    public void salvar(Estado estado){
+    @PostMapping
+    public void salvar(@RequestBody Estado estado){
         repositorioEstado.save(estado);
     }
 
-    public void alterar(Estado estado){
+    @PutMapping
+    public void alterar(@RequestBody Estado estado){
         if(estado.getIdEstado() > 0)
         repositorioEstado.save(estado);
     }
 
-    public void excluir(Estado estado){
+    @DeleteMapping
+    public void excluir(@RequestBody Estado estado){
         repositorioEstado.delete(estado);
     }
 }
